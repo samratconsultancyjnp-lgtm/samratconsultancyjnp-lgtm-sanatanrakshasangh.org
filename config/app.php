4ba06bf759dc3c6d07b421eac0486982218d60d3
samratconsultancyjnp-lgtm <277428030+samratconsultancyjnp-lgtm@users.noreply.github.com>
2026-04-26 14:19:29 +0530

    apiupdate

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:/aZvyvInK59QZuNrCtp0kMhTlF1wr/2LM9bBwYbQwKo='),
 
     'previous_keys' => [
         ...array_filter(
